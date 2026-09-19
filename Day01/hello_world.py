275ba7667be657d108fc10bd83877e6e943f6cca
srk <mr.shahrukh129@gmail.com>
2026-09-19 16:07:36 +0530

    Fix Day 1 and Day 2 repository structure

---
+++ Day01/hello_world.py
@@ -0,0 +1,9 @@
+    # Day 1 - Python Basics
+
+print("Hello, World!")
+
+print("My name is Shahrukh khan.")
+print("I am learning Python.")
+print("This is Day 1 of my Python challenge.")
+print("I will code every day and push my progress to GitHub.")
+print("I am building my coding consistency.")
---
+++ Day02/variables_datatypes.py
@@ -0,0 +1,23 @@
+# Day 2 - Variables and Data Types
+
+# Variables
+
+name = "Shahrukh"
+age = 22
+height = 5.4
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+
+# Data Types
+
+print("\nData Types:")
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
